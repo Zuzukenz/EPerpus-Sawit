e139f8d56c070b9da2fbc1071e644c02b62a4c94
FΔR!! <bagassutoyo759@gmail.com>
2026-08-19 15:23:07 +0700

    fix: update dashboard to use API endpoint for books instead of web route

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -33,8 +33,8 @@
                     </a>
                 </li>
                 <li>
-                    <a href="{{ route('books.index') }}" class="block px-4 py-2 rounded-lg text-gray-700 hover:bg-gray-100">
-                        📚 Kelola Buku
+                    <a href="javascript:void(0);" onclick="alert('Kelola Buku tersedia melalui API: /api/books')" class="block px-4 py-2 rounded-lg text-gray-700 hover:bg-gray-100 cursor-pointer">
+                        📚 Kelola Buku (API)
                     </a>
                 </li>
                 <li>
